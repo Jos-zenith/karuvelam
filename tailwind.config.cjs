e361800e200d5d4af6558d1a313571e5e2c805d8
Zenith Joshua <145341868+Jos-zenith@users.noreply.github.com>
2026-01-02 21:03:34 +0530

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        forest: "#1A3C34",
+        earth: "#4B3621",
+        techblue: "#00A8E8",
+      },
+    },
+  },
+  plugins: [],
+};
